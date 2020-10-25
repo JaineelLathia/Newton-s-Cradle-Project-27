b610f192d919183851b97f928aef94feafb11b0a
JaineelLathia <71533952+JaineelLathia@users.noreply.github.com>
2020-10-25 09:24:12 -0700

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,24 @@
+class bob {
+    constructor(x, y, radius) {
+      var options = {
+          isStatic:false,
+          'restitution':0.9,
+          'friction':0.9,
+          'density':1.2
+      }
+      //this.image = loadImage("paper.png");
+      this.radius = radius;
+      this.x = x;
+      this.y = y;
+      this.body = Bodies.circle(this.x, this.y, (this.radius)/2, options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos=this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      fill("gray");
+      circle(0,0, this.radius)
+      pop(); 
+    }
+  }
